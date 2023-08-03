21dd038066f66802daf77245aa72642af8789a3d
aiueokakikukekosasisu <141223973+aiueokakikukekosasisu@users.noreply.github.com>
2023-08-03 21:12:02 +0900

    Impostor.cs

--- Roles/Crewmate/Doctor.cs
+++ Roles/Crewmate/Doctor.cs
@@ -3,43 +3,69 @@ using AmongUs.GameOptions;
 using TownOfHost.Roles.Core;
 using TownOfHost.Roles.Core.Interfaces;
 
-namespace TownOfHost.Roles.Crewmate;
-public sealed class Doctor : RoleBase, IDeathReasonSeeable
+namespace TownOfHost.Roles.Impostor
 {
-    public static readonly SimpleRoleInfo RoleInfo =
-        SimpleRoleInfo.Create(
-            typeof(Doctor),
-            player => new Doctor(player),
-            CustomRoles.Doctor,
-            () => RoleTypes.Scientist,
-            CustomRoleTypes.Crewmate,
-            20700,
-            SetupOptionItem,
-            "doc",
-            "#80ffdd"
-        );
-    public Doctor(PlayerControl player)
-    : base(
-        RoleInfo,
-        player
-    )
+    public sealed class TimeThief : RoleBase, IMeetingTimeAlterable, IImpostor
     {
-        TaskCompletedBatteryCharge = OptionTaskCompletedBatteryCharge.GetFloat();
-    }
-    private static OptionItem OptionTaskCompletedBatteryCharge;
-    enum OptionName
-    {
-        DoctorTaskCompletedBatteryCharge
-    }
-    private static float TaskCompletedBatteryCharge;
-    private static void SetupOptionItem()
-    {
-        OptionTaskCompletedBatteryCharge = FloatOptionItem.Create(RoleInfo, 10, OptionName.DoctorTaskCompletedBatteryCharge, new(0f, 10f, 1f), 5f, false)
-            .SetValueFormat(OptionFormat.Seconds);
-    }
-    public override void ApplyGameOptions(IGameOptions opt)
-    {
-        AURoleOptions.ScientistCooldown = 0f;
-        AURoleOptions.ScientistBatteryCharge = TaskCompletedBatteryCharge;
+        public static readonly SimpleRoleInfo RoleInfo =
+            SimpleRoleInfo.Create(
+                typeof(TimeThief),
+                player => new TimeThief(player),
+                CustomRoles.TimeThief,
+                () => RoleTypes.Impostor,
+                CustomRoleTypes.Impostor,
+                2400,
+                SetupOptionItem,
+                "tt"
+            );
+        public TimeThief(PlayerControl player)
+        : base(
+            RoleInfo,
+            player
+        )
+        {
+            KillCooldown = OptionKillCooldown.GetFloat();
+            DecreaseMeetingTime = OptionDecreaseMeetingTime.GetInt();
+            LowerLimitVotingTime = OptionLowerLimitVotingTime.GetInt();
+            ReturnStolenTimeUponDeath = OptionReturnStolenTimeUponDeath.GetBool();
+        }
+        private static OptionItem OptionKillCooldown;
+        private static OptionItem OptionDecreaseMeetingTime;
+        private static OptionItem OptionLowerLimitVotingTime;
+        private static OptionItem OptionReturnStolenTimeUponDeath;
+        enum OptionName
+        {
+            TimeThiefDecreaseMeetingTime,
+            TimeThiefLowerLimitVotingTime,
+            TimeThiefReturnStolenTimeUponDeath
+        }
+        public static float KillCooldown;
+        public static int DecreaseMeetingTime;
+        public static int LowerLimitVotingTime;
+        public static bool ReturnStolenTimeUponDeath;
+
+        public bool RevertOnDie => ReturnStolenTimeUponDeath;
+
+        private static void SetupOptionItem()
+        {
+            OptionKillCooldown = FloatOptionItem.Create(RoleInfo, 10, GeneralOption.KillCooldown, new(2.5f, 180f, 2.5f), 30f, false)
+                .SetValueFormat(OptionFormat.Seconds);
+            OptionDecreaseMeetingTime = IntegerOptionItem.Create(RoleInfo, 11, OptionName.TimeThiefDecreaseMeetingTime, new(0, 100, 1), 20, false)
+                .SetValueFormat(OptionFormat.Seconds);
+            OptionLowerLimitVotingTime = IntegerOptionItem.Create(RoleInfo, 12, OptionName.TimeThiefLowerLimitVotingTime, new(1, 300, 1), 10, false)
+                .SetValueFormat(OptionFormat.Seconds);
+            OptionReturnStolenTimeUponDeath = BooleanOptionItem.Create(RoleInfo, 13, OptionName.TimeThiefReturnStolenTimeUponDeath, true, false);
+        }
+        public float CalculateKillCooldown() => KillCooldown;
+        public int CalculateMeetingTimeDelta()
+        {
+            var sec = -(DecreaseMeetingTime * MyState.GetKillCount(true));
+            return sec;
+        }
+        public override string GetProgressText(bool comms = false)
+        {
+            var time = CalculateMeetingTimeDelta();
+            return time < 0 ? Utils.ColorString(Palette.ImpostorRed.ShadeColor(0.5f), $"{time}s") : "";
+        }
     }
 }
